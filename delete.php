a81b877071372033456ff718b2139a6aca31fff7
Repro <ha.briann@gmail.com>
2021-12-19 15:47:05 -0500

    added delete code

---
+++ delete.php
@@ -0,0 +1,31 @@
+<?php
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+$query = "SELECT * FROM comment";
+  mysqli_query($conn, $query) or die('Error querying database');
+
+$id = $_GET['id'];
+
+$delete = mysqli_query($conn, "DELETE FROM comment WHERE id = '$id'");
+
+if($delete){
+    mysqli_close($db);
+    header("location:index.php");
+    exit;
+}
+else{
+    echo "Error deleting record";
+}
+
+
+?>
