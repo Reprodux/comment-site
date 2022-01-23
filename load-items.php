df5abf04ca9055339a1ccf4e007a0ced2e95b4bf
Repro <ha.briann@gmail.com>
2022-01-23 13:30:38 -0500

    edited button

--- load-items.php
+++ load-items.php
@@ -19,12 +19,12 @@
         echo "<tr>";
         $id = "delete.php?id=";
         $id .= $row['id'];
-        echo "<td><a id='button' class= 'button' href='$id' >Delete Entry</a></td>";
-        //echo "<td><button id=";
-        //echo $row['id'];
-        //echo " onclick='delete_data('";
-        //echo $row['id'];
-        //echo ")'p>Delete Entry</button></td>";
+        //echo "<td><a id='button' class= 'button' href='$id' >Delete Entry</a></td>";
+        echo "<td><a id=";
+        echo $row['id'];
+        echo " class ='button' onclick='delete_data('";
+        echo $row['id'];
+        echo ")'p>Delete Entry</a></td>";
         echo "</tr>";
     }
     
