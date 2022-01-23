9dbee383e94de979264fe6c4ce08ddeaf93d26eb
Repro <ha.briann@gmail.com>
2022-01-23 11:06:40 -0500

    editied

--- delete.php
+++ delete.php
@@ -15,9 +15,9 @@ $query = "SELECT * FROM to_do";
   mysqli_query($conn, $query) or die('Error querying database');
 
 $id = $_POST['id'];
-$delete = mysqli_query($conn, "DELETE FROM to_do WHERE id = '$id'")or die(mysql_error());
+//$delete = mysqli_query($conn, "DELETE FROM to_do WHERE id = '$id'")or die(mysql_error());
 
-/*
+
 $delete = mysqli_query($conn, "DELETE FROM to_do WHERE id = '$id'");
 
 echo $delete;
@@ -30,6 +30,6 @@ if($delete){
 else{
     echo "Error deleting record";
 }
-*/
+
 
 ?>
